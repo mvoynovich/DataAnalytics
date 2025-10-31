02d48efd3b7445a380763bf9cbdfd7893bfbf147
mvoynovich <143434817+mvoynovich@users.noreply.github.com>
2025-10-31 02:01:34 -0400

    Finish lab4

--- Lab4/Data_Analytics2025Fall_Lab4_code.R
+++ Lab4/Data_Analytics2025Fall_Lab4_code.R
@@ -12,7 +12,7 @@ library(psych)
 library(readr)
 
 ## set working directory so that files can be referenced without the full path
-setwd("~/Courses/Data Analytics/Fall25/labs/lab 4/")
+setwd("C:/Users/matec/DataAnalytics/Lab4")
 
 ## read dataset
 wine <- read_csv("wine.data", col_names = FALSE)
@@ -38,4 +38,148 @@ pairs.panels(wine[,-1],gap = 0,bg = c("red", "yellow", "blue")[wine$Type],pch=21
 
 ggpairs(wine, ggplot2::aes(colour = Type))
 
-###
+### use prcomp since its the modern version of princomp
+Xmat <- as.matrix(wine[, -1])
+Xc <- scale(Xmat, center = TRUE, scale = TRUE)
+
+pca <- prcomp(Xc)
+summary(pca)
+
+pc_scores <- pca$x[, 1:2]
+head(pc_scores)
+
+pc_loadings <- pca$rotation[, 1:2]
+pc_loadings
+
+plot(pca, type = "l")
+biplot(pca)
+
+autoplot(pca, data = wine, colour = 'Type',
+         loadings = TRUE, loadings.colour = 'blue',
+         loadings.label = TRUE, loadings.label.size = 3, scale = 0)
+
+# Do KNN for all variables
+split.rat <- 0.7
+train.indexes <- sample(178,split.rat*178)
+
+train <- wine[train.indexes,]
+test <- wine[-train.indexes,]
+
+ctrl <- trainControl(method = "cv", number = 10)
+
+knn.all <- train(Type~., data=train, 
+                         method="knn",  preProcess = c("center", "scale"), metric="Accuracy", trControl=ctrl)
+
+knn.train.true <- train$Type
+knn.test.true <- test$Type
+
+knn.train.predicted <- predict(knn.all,train[,-1])
+knn.test.predicted <- predict(knn.all,test[,-1])
+
+train.cm = as.matrix(table(Actual = knn.train.true, Predicted = knn.train.predicted))
+train.cm
+train.accuracy <- sum(diag(train.cm))/nrow(train) 
+train.accuracy
+
+n = sum(train.cm) # number of instances
+nc = nrow(train.cm) # number of classes
+diag = diag(train.cm) # number of correctly classified instances per class 
+rowsums = apply(train.cm, 1, sum) # number of instances per class
+colsums = apply(train.cm, 2, sum) # number of predictions per class
+p = rowsums / n # distribution of instances over the actual classes
+q = colsums / n # distribution of instances over the predicted 
+
+accuracy = sum(diag)/n
+accuracy
+
+precision = diag / colsums
+recall = diag / rowsums 
+f1 = 2 * precision * recall / (precision + recall) 
+
+data.frame(recall, precision, f1)
+
+test.cm = as.matrix(table(Actual = knn.test.true, Predicted = knn.test.predicted))
+test.cm 
+test.accuracy <- sum(diag(test.cm))/nrow(test)
+test.accuracy
+
+n = sum(test.cm) # number of instances
+nc = nrow(test.cm) # number of classes
+diag = diag(test.cm) # number of correctly classified instances per class 
+rowsums = apply(test.cm, 1, sum) # number of instances per class
+colsums = apply(test.cm, 2, sum) # number of predictions per class
+p = rowsums / n # distribution of instances over the actual classes
+q = colsums / n # distribution of instances over the predicted 
+
+accuracy = sum(diag)/n
+accuracy
+
+precision = diag / colsums
+recall = diag / rowsums 
+f1 = 2 * precision * recall / (precision + recall) 
+
+data.frame(recall, precision, f1)
+
+# Do KNN for the first two principle components
+pc.data <- as.data.frame(pca$x[, 1:2])
+pc.data$Type <- as.factor(wine$Type)
+
+split.rat <- 0.7
+train.indexes <- sample(178,split.rat*178)
+
+train <- pc.data[train.indexes,]
+test <- pc.data[-train.indexes,]
+
+knn.pca <- train(Type~., data=train, 
+                 method="knn",  preProcess = c("center", "scale"), metric="Accuracy", trControl=ctrl)
+
+knn.train.true <- train$Type
+knn.test.true <- test$Type
+
+knn.train.predicted <- predict(knn.pca,train[,-3])
+knn.test.predicted <- predict(knn.pca,test[,-3])
+
+train.cm = as.matrix(table(Actual = knn.train.true, Predicted = knn.train.predicted))
+train.cm
+train.accuracy <- sum(diag(train.cm))/nrow(train) 
+train.accuracy
+
+n = sum(train.cm) # number of instances
+nc = nrow(train.cm) # number of classes
+diag = diag(train.cm) # number of correctly classified instances per class 
+rowsums = apply(train.cm, 1, sum) # number of instances per class
+colsums = apply(train.cm, 2, sum) # number of predictions per class
+p = rowsums / n # distribution of instances over the actual classes
+q = colsums / n # distribution of instances over the predicted 
+
+accuracy = sum(diag)/n
+accuracy
+
+precision = diag / colsums
+recall = diag / rowsums 
+f1 = 2 * precision * recall / (precision + recall) 
+
+data.frame(recall, precision, f1)
+
+test.cm = as.matrix(table(Actual = knn.test.true, Predicted = knn.test.predicted))
+test.cm 
+test.accuracy <- sum(diag(test.cm))/nrow(test)
+test.accuracy
+
+n = sum(test.cm) # number of instances
+nc = nrow(test.cm) # number of classes
+diag = diag(test.cm) # number of correctly classified instances per class 
+rowsums = apply(test.cm, 1, sum) # number of instances per class
+colsums = apply(test.cm, 2, sum) # number of predictions per class
+p = rowsums / n # distribution of instances over the actual classes
+q = colsums / n # distribution of instances over the predicted 
+
+accuracy = sum(diag)/n
+accuracy
+
+precision = diag / colsums
+recall = diag / rowsums 
+f1 = 2 * precision * recall / (precision + recall) 
+
+data.frame(recall, precision, f1)
+
